5423ec44b36243583a1e7a58bb0a12a7a4db3cf1
coopTRUE <coopdog4545@gmail.com>
2023-03-12 08:02:37 -0700

    added prettier and starter files

---
+++ src/constants/networks.ts
@@ -0,0 +1,10 @@
+export default {
+  ethereumMainnet: {
+    chainId: 1,
+    rpcs: {
+      ankr: 'https://rpc.ankr.com/eth',
+      cloudflare: 'https://cloudflare-eth.com'
+    },
+    explorer: 'https://etherscan.io'
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
-export function add(a: boolean, b: number) {
-  return a + b
+import { ethers } from 'ethers'
+import NETWORKS from './constants/networks'
+
+export default class TokenReceiver {
+  private provider: ethers.JsonRpcProvider
+  private contract: ethers.Contract
+  private contractAddress: string
+
+  constructor(provider: ethers.JsonRpcProvider, contractAddress: string) {
+    this.provider = provider
+    this.contractAddress = contractAddress
+  }
+
+  public async init() {
+    const abi = [
+      'event Transfer(address indexed from, address indexed to, uint256 value)'
+    ]
+    this.contract = new ethers.Contract(
+      this.contractAddress,
+      abi,
+      this.provider
+    )
+  }
+
+  public async listen() {
+    this.contract.on('Transfer', (from, to, value) => {
+      console.log(from, to, value)
+    })
+  }
 }
